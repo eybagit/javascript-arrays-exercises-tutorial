7c24d8b397c156b0a10258bc14312bce3f7bd0aa
Elkin Botero <147097939+eybagit@users.noreply.github.com>
2025-05-16 03:19:04 +0000

    ArreglosEnJavascript1

--- .learn/resets/08.2-Count-On/app.js
+++ .learn/resets/08.2-Count-On/app.js
@@ -3,9 +3,7 @@ let myArray = [42, true, "towel", [2,1], 'hello', 34.4, {"name": "juan"}];
 let hello = [];
 for(let i = 0; i < myArray.length; i++) {
     let item = myArray[i];
-       // console.log(typeof(item));
-    if (typeof(myArray[i]) == "object" ) 
-        hello.push(myArray[i]);
+    // MAGIC HAPPENS HERE
 }
 
-console.log(hello);
+console.log(hello)
--- exercises/08.1-Mixed-array/app.js
+++ exercises/08.1-Mixed-array/app.js
@@ -1,3 +1,13 @@
 let mix = [42, true, "towel", [2,1], 'hello', 34.4, {"name": "juan"}];
 
+function tipoDeDato(mix){
+    let tipo = [];
+    for (let i = 0; i < mix.length; i++) {
+        tipo.push(typeof(mix[i]));
+    }
+    return tipo;
+}
+
+console.log(tipoDeDato(mix));
+
 // Your code here
