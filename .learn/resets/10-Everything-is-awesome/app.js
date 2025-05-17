564765e4a7b54cea35f9beb9875a58b3d890da92
Elkin Botero <147097939+eybagit@users.noreply.github.com>
2025-05-17 05:21:52 +0000

    ArreglosEnJavascript1

--- .learn/resets/10-Everything-is-awesome/app.js
+++ .learn/resets/10-Everything-is-awesome/app.js
@@ -3,9 +3,7 @@ let myArray = [ 1, 0, 0, 0, 1, 0, 0, 0, 1, 1 ];
 const ZerosToYahoos = (arr) => {
     let return_array = [];
     arr.forEach((item) => {
-            if (item === 0) {
-                return_array.push('Yahoo');
-            }else return_array.push(item);
+            // magic goes inside these brackets
     });
     return return_array;
 };
